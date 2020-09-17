69a502c89351230fdb73974dabd2200a4bf9af85
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-09-17 13:52:52 +0300

    FIR BE: throw exception on getting ConeCapturedType
    This commit assumes that captured types are forbidden
    at the end of resolve

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrTypeConverter.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrTypeConverter.kt
@@ -101,7 +101,7 @@ class Fir2IrTypeConverter(
                 upperBound.toIrType(typeContext)
             }
             is ConeCapturedType -> {
-                lowerType?.toIrType(typeContext) ?: constructor.supertypes!!.first().toIrType(typeContext)
+                throw IllegalStateException("Captured types should not persist at the end of resolve: ${this.render()}")
             }
             is ConeDefinitelyNotNullType -> {
                 original.toIrType(typeContext.definitelyNotNull())
