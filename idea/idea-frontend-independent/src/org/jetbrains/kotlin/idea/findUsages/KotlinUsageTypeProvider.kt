16d558e13b39d80fe13294981060eeb023941189
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2020-09-18 03:19:34 +0300

    APPEND TO USAGE TYPE PROVIDER

--- idea/idea-frontend-independent/src/org/jetbrains/kotlin/idea/findUsages/KotlinUsageTypeProvider.kt
+++ idea/idea-frontend-independent/src/org/jetbrains/kotlin/idea/findUsages/KotlinUsageTypeProvider.kt
@@ -9,7 +9,7 @@ import com.intellij.psi.PsiElement
 import com.intellij.usages.UsageTarget
 import com.intellij.usages.impl.rules.UsageType
 import com.intellij.usages.impl.rules.UsageTypeProviderEx
-import org.jetbrains.kotlin.idea.KotlinBundleIndependent
+import org.jetbrains.kotlin.idea.findUsages.KotlinUsageTypes.toUsageType
 import org.jetbrains.kotlin.idea.findUsages.UsageTypeEnum.*
 import org.jetbrains.kotlin.idea.references.*
 import org.jetbrains.kotlin.psi.*
@@ -38,7 +38,7 @@ abstract class KotlinUsageTypeProvider : UsageTypeProviderEx {
 
     override fun getUsageType(element: PsiElement?, targets: Array<out UsageTarget>): UsageType? {
         val usageType = getUsageTypeEnum(element) ?: return null
-        return convertEnumToUsageType(usageType)
+        return usageType.toUsageType()
     }
 
     private fun getCommonUsageType(refExpr: KtReferenceExpression): UsageTypeEnum? = when {
@@ -137,82 +137,6 @@ abstract class KotlinUsageTypeProvider : UsageTypeProviderEx {
         refExpr.getNonStrictParentOfType<KtQualifiedExpression>() != null -> PACKAGE_MEMBER_ACCESS
         else -> getClassUsageType(refExpr)
     }
-
-    private fun convertEnumToUsageType(usageType: UsageTypeEnum): UsageType = when (usageType) {
-        TYPE_CONSTRAINT -> KotlinUsageTypes.TYPE_CONSTRAINT
-        VALUE_PARAMETER_TYPE -> KotlinUsageTypes.VALUE_PARAMETER_TYPE
-        NON_LOCAL_PROPERTY_TYPE -> KotlinUsageTypes.NON_LOCAL_PROPERTY_TYPE
-        FUNCTION_RETURN_TYPE -> KotlinUsageTypes.FUNCTION_RETURN_TYPE
-        SUPER_TYPE -> KotlinUsageTypes.SUPER_TYPE
-        IS -> KotlinUsageTypes.IS
-        CLASS_OBJECT_ACCESS -> KotlinUsageTypes.CLASS_OBJECT_ACCESS
-        COMPANION_OBJECT_ACCESS -> KotlinUsageTypes.COMPANION_OBJECT_ACCESS
-        EXTENSION_RECEIVER_TYPE -> KotlinUsageTypes.EXTENSION_RECEIVER_TYPE
-        SUPER_TYPE_QUALIFIER -> KotlinUsageTypes.SUPER_TYPE_QUALIFIER
-        TYPE_ALIAS -> KotlinUsageTypes.TYPE_ALIAS
-
-        FUNCTION_CALL -> KotlinUsageTypes.FUNCTION_CALL
-        IMPLICIT_GET -> KotlinUsageTypes.IMPLICIT_GET
-        IMPLICIT_SET -> KotlinUsageTypes.IMPLICIT_SET
-        IMPLICIT_INVOKE -> KotlinUsageTypes.IMPLICIT_INVOKE
-        IMPLICIT_ITERATION -> KotlinUsageTypes.IMPLICIT_ITERATION
-        PROPERTY_DELEGATION -> KotlinUsageTypes.PROPERTY_DELEGATION
-
-        RECEIVER -> KotlinUsageTypes.RECEIVER
-        DELEGATE -> KotlinUsageTypes.DELEGATE
-
-        PACKAGE_DIRECTIVE -> KotlinUsageTypes.PACKAGE_DIRECTIVE
-        PACKAGE_MEMBER_ACCESS -> KotlinUsageTypes.PACKAGE_MEMBER_ACCESS
-
-        CALLABLE_REFERENCE -> KotlinUsageTypes.CALLABLE_REFERENCE
-
-        READ -> UsageType.READ
-        WRITE -> UsageType.WRITE
-        CLASS_IMPORT -> UsageType.CLASS_IMPORT
-        CLASS_LOCAL_VAR_DECLARATION -> UsageType.CLASS_LOCAL_VAR_DECLARATION
-        TYPE_PARAMETER -> UsageType.TYPE_PARAMETER
-        CLASS_CAST_TO -> UsageType.CLASS_CAST_TO
-        ANNOTATION -> UsageType.ANNOTATION
-        CLASS_NEW_OPERATOR -> UsageType.CLASS_NEW_OPERATOR
-        NAMED_ARGUMENT -> KotlinUsageTypes.NAMED_ARGUMENT
-
-        USAGE_IN_STRING_LITERAL -> UsageType.LITERAL_USAGE
-    }
-}
-
-object KotlinUsageTypes {
-    // types
-    val TYPE_CONSTRAINT = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.type.constraint"))
-    val VALUE_PARAMETER_TYPE = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.value.parameter.type"))
-    val NON_LOCAL_PROPERTY_TYPE = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.nonLocal.property.type"))
-    val FUNCTION_RETURN_TYPE = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.function.return.type"))
-    val SUPER_TYPE = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.superType"))
-    val IS = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.is"))
-    val CLASS_OBJECT_ACCESS = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.class.object"))
-    val COMPANION_OBJECT_ACCESS = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.companion.object"))
-    val EXTENSION_RECEIVER_TYPE = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.extension.receiver.type"))
-    val SUPER_TYPE_QUALIFIER = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.super.type.qualifier"))
-    val TYPE_ALIAS = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.type.alias"))
-
-    // functions
-    val FUNCTION_CALL = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.function.call"))
-    val IMPLICIT_GET = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.implicit.get"))
-    val IMPLICIT_SET = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.implicit.set"))
-    val IMPLICIT_INVOKE = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.implicit.invoke"))
-    val IMPLICIT_ITERATION = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.implicit.iteration"))
-    val PROPERTY_DELEGATION = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.property.delegation"))
-
-    // values
-    val RECEIVER = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.receiver"))
-    val DELEGATE = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.delegate"))
-
-    // packages
-    val PACKAGE_DIRECTIVE = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.packageDirective"))
-    val PACKAGE_MEMBER_ACCESS = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.packageMemberAccess"))
-
-    // common usage types
-    val CALLABLE_REFERENCE = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.callable.reference"))
-    val NAMED_ARGUMENT = UsageType(KotlinBundleIndependent.lazyMessage("find.usages.type.named.argument"))
 }
 
 enum class UsageTypeEnum {
